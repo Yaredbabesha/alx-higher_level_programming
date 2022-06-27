206002a4d1655d1b3b976431b23d76b39b5957e3
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-06-27 13:12:08 -0900

    modified:   6-rectangle.py git commit -m "My commit message"

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -54,4 +54,31 @@ class Rectangle:
         height = self.__height
         if width == 0 or height == 0:
             return 0
+        return (width + height) * 2
 
+    def __str__(self):
+        """Get string representation"""
+        width = self.__width
+        height = self.__height
+        string = ""
+        if width == 0 or height == 0:
+            return string
+        for r in range(height):
+            for c in range(width):
+                string = string + '#'
+            string = string + '\n'
+        return string[:-1]
+
+    def __repr__(self):
+        """Get string."""
+        width = self.__width
+        height = self.__height
+        string = "Rectangle(" + str(width) + \
+            ", " + str(height) + ")"
+        return string
+
+    def __del__(self):
+        """deleted"""
+        if Rectangle.number_of_instances > 0:
+            Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
