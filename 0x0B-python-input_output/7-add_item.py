cc817132889d02bc95190886502b3081e93c0af5
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-07-05 11:48:56 -0900

    modified:   7-add_item.py git commit -m "My commit message"

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-    module contains:
-        script that adds all arguments to a Python list
-        and then saves them to a file
+    This is a python script that adds all arguments to a Python List.
+    List is then saved to a file.
 """
-import sys
 
-if __name__ == '__main__':
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+
+    filename = "add_item.json"
 
     try:
-        content = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        content = []
-    content.extend(sys.argv[1:])
-    save_to_json_file(content, 'add_item.json')
+        arg_list = load_from_json_file(filename)
+    except:
+        arg_list = []
+
+    for arg in sys.argv[1:]:
+        arg_list.append(arg)
+    save_to_json_file(arg_list, filename)
