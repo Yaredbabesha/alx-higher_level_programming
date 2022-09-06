4525a361ea5d54b5e9c8623e746817646e3e8af7
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-09-06 06:22:08 -0900

    modified:   102-concat.js git commit -m "js objects"

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const fileA = (process.argv[2].toString());
-const fileB = (process.argv[3].toString());
-const data = fs.readFileSync(fileA, 'utf-8');
-const datab = fs.readFileSync(fileB, 'utf-8');
-const msg = data + datab;
-fs.writeFile(process.argv[4].toString(), msg, function (err) {
-  if (err) {
-    return console.log(err);
-  }
+
+const fileA = fs.readFileSync(process.argv[2]);
+const fileB = fs.readFileSync(process.argv[3]);
+
+fs.writeFile(process.argv[4], fileA + fileB, function (err) {
+  if (err) throw err;
 });
